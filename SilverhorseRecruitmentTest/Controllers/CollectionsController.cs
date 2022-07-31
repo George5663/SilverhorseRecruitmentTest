8a128bd9729e4337af9ec1bef534e5c7498a16e7
George4788 <20221577@student.curtin.edu.au>
2022-08-01 03:35:39 +0800

    Add project files.

---
+++ SilverhorseRecruitmentTest/Controllers/CollectionsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SilverhorseRecruitmentTest.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SilverhorseRecruitmentTest.Controllers
+{
+    [Route("/collections")]
+    public class CollectionsController : Controller
+    {
+        [HttpGet]
+        [Authorize]
+        //Return the collection of posts/albums/users
+        public async Task<IActionResult> GetAction()
+        {
+            try
+            {
+                var collection = await GetData.Collections();
+
+                return Ok(collection);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(501, ex.Message.ToString());
+            }
+        }
+    }
+}
---
+++ SilverhorseRecruitmentTest/Controllers/PostsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using System.Web.Http;
+using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
+
+namespace SilverhorseRecruitmentTest.Controllers
+{
+    //Controller for Post CRUD routes
+    [Route("api/posts")]
+    [ApiController]
+    public class PostsController : ControllerBase
+    {
+        //Get all posts
+        // GET: api/<PostsController>
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> GetPosts()
+        {
+
+        }
+
+    }
+}
